9509e5cb3b5abaa4746da89cb649c24db137c7ae
Shane Poppleton <shane@bjja.com.au>
2021-04-21 11:45:30 +1000

    First commit

---
+++ resources/js/Jetstream/Button.vue
@@ -0,0 +1,16 @@
+<template>
+    <button :type="type" class="inline-flex items-center px-4 py-2 bg-brand-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-brand-700 active:bg-brand-900 focus:outline-none focus:border-brand-900 focus:ring focus:ring-brand-300 disabled:opacity-25 transition">
+        <slot></slot>
+    </button>
+</template>
+
+<script>
+    export default {
+        props: {
+            type: {
+                type: String,
+                default: 'submit',
+            },
+        }
+    }
+</script>
---
+++ tailwind.config.js
@@ -0,0 +1,42 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+    purge: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './vendor/laravel/jetstream/**/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+        './resources/js/**/*.vue',
+    ],
+
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                brand: {
+                    50: '#F4ECFC',
+                    100: '#DBC9F0',
+                    200: '#BCA1DC',
+                    300: '#A58DC2',
+                    400: '#927BAF',
+                    500: '#80699D',
+                    600: '#7B639A',
+                    700: '#6E5192',
+                    800: '#5E3B8A',
+                    900: '#3B205B'
+                }
+            }
+        },
+        
+    },
+
+    variants: {
+        extend: {
+            opacity: ['disabled'],
+        },
+    },
+
+    plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
+};
