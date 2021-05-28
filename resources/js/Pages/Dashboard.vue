5e0b3c214034e9ac7fa9d5c48595ab5f1f4d81cf
Shane Poppleton <shane@bjja.com.au>
2021-05-28 12:23:17 +1000

    Added null handling of stats

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -9,10 +9,10 @@
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div v-if="true" class="flex justify-between flex-wrap">
-                    <billables :billable-hours="$page.props.stats.billable_hours"/>
-                    <billing-tickets :billable-tickets="$page.props.stats.billable_tickets"/>
-                    <creation-rate :creation-rate="$page.props.stats.ticket_creation_rate"/>
-                    <open-tickets :open-tickets="$page.props.stats.open_tickets" />
+                    <billables :billable-hours="$page.props.stats?.billable_hours"/>
+                    <billing-tickets :billable-tickets="$page.props.stats?.billable_tickets"/>
+                    <creation-rate :creation-rate="$page.props.stats?.ticket_creation_rate"/>
+                    <open-tickets :open-tickets="$page.props.stats?.open_tickets" />
                 </div>
                 <div class="dark:bg-gray-800 bg-white overflow-hidden shadow-xl sm:rounded-lg">
                     <welcome/>
