8c8c0e9e9fca73b32129010188f609e8a07d70f5
Shane Poppleton <shane@alphasg.com.au>
2021-06-03 11:06:03 +1000

    switch from Plivo to Twilio

--- app/Notifications/VerifyPhone.php
+++ app/Notifications/VerifyPhone.php
@@ -6,27 +6,23 @@ use Codemonkey76\Plivo\PlivoChannel;
 use Codemonkey76\Plivo\PlivoMessage;
 use Illuminate\Notifications\Messages\NexmoMessage;
 use Illuminate\Notifications\Notification;
+use NotificationChannels\Twilio\TwilioChannel;
+use NotificationChannels\Twilio\TwilioSmsMessage;
 
 
 class VerifyPhone extends Notification
 {
     public function via(mixed $notifiable): array|string
     {
-        return [PlivoChannel::class];
+        return [TwilioChannel::class];
     }
 
-    public function toPlivo($notifiable)
+    public function toTwilio($notifiable)
     {
         $validity = config('app.defaults.sms_expiry') / 60;
 
-        return (new PlivoMessage)
-            ->content("Alpha IT Centre Helpdesk Code: {$notifiable->phone_verification_code}. Valid for {$validity} minutes.");
-    }
-    public function toNexmo($notifiable)
-    {
-        $validity = config('app.defaults.sms_expiry') / 60;
-
-        return (new NexmoMessage)
+        return (new TwilioSmsMessage)
+            ->sender('AlphaIT')
             ->content("Alpha IT Centre Helpdesk Code: {$notifiable->phone_verification_code}. Valid for {$validity} minutes.");
     }
 }
